e2c5ce54b793e693da17158cf4e5cad1446d5578
Sus1221 <susanne881221@hotmail.com>
2014-12-31 14:36:20 +0100

    Removed some files and managed to make a connection to the db with swiss knife-helper

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once("nodebite-swiss-army-oop.php");
+
+$ds = new DBObjectSaver(array(
+    "host" => "127.0.0.1",
+	"dbname" => "wu14oop2",
+	"username" => "root",
+	"password" => "mysql",
+	"prefix" => "CarFanatics" // a prefix unique for your app/project
+	));
+
+$ds->persons["Susanne"] = new Character("Susanne");
